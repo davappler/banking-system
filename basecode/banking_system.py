7fa0dec17199ca734a39b806997ab0da3b415fb8
davibaweja <davibaweja@gmail.com>
2022-07-22 19:06:16 +0100

    added some input functions

--- basecode/banking_system.py
+++ basecode/banking_system.py
@@ -6,13 +6,22 @@
 class BankingSystem:
 
     def __init__(self):
-        self.userName=
+
         # Do not add any parameter to this method.
         # Delete "pass" after adding code into this method.
         pass
 
     def run_app(self):
         print("Your banking system should run by calling this method.")
+        username = input("What is your username? ")
+        users_password = input("What is your password? ")
+
+    def user_logged_in():
+        print("Please select an option:")
+        print(" 1 - Customer summary")
+        print(" 2 - Financial summary")
+        print(" 3 - Quit")
+        user_input = input("Enter a number for your option: ")
 
 
 

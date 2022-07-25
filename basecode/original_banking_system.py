d2664d1c0247d3c649ccbfdaeb8486fc16661eec
davibaweja <davibaweja@gmail.com>
2022-07-25 23:14:57 +0100

    renamed files

---
+++ basecode/original_banking_system.py
@@ -0,0 +1,37 @@
+############## BankingSystem ################
+# Student Name:
+# Student ID:
+######################################
+
+class BankingSystem:
+
+    def __init__(self):
+
+        # Do not add any parameter to this method.
+        # Delete "pass" after adding code into this method.
+        pass
+
+    def run_app(self):
+        print("Your banking system should run by calling this method.")
+        username = input("What is your username? ")
+        users_password = input("What is your password? ")
+
+    def user_logged_in():
+        print("Please select an option:")
+        print(" 1 - Customer summary")
+        print(" 2 - Financial summary")
+        print(" 3 - Quit")
+        user_input = input("Enter a number for your option: ")
+
+
+
+p1=BankingSystem()
+p1.run_app()
+
+
+
+
+
+
+# Customer and Admin
+# current and savings account
